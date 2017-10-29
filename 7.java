49be24bab6d088bd41ae8c6df1bba09c9e4be108
GitYarKa <33204604+GitYarKa@users.noreply.github.com>
2017-10-30 00:48:57 +0300

    Update 7.java

--- 7.java
+++ 7.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 public class Task7 {
 
 	public static void main(String[] args) {
-		String[] s = {"Ìóð", "Âæóõ", "Áóëü"};
+		String[] s = {"Крылья", "ИТМО", "Вперед"};
 		Arrays.sort(s);
 		for (int i=0; i<s.length;i++)
 			System.out.println(s[i]);

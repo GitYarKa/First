f28028f293ac6fa256ea9bc1108272e525728def
GitYarKa <33204604+GitYarKa@users.noreply.github.com>
2017-10-30 00:16:08 +0300

    Update 1.java

--- 1.java
+++ 1.java
@@ -4,15 +4,15 @@ public class Task1 {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		int a, b, abuf, bbuf;
-		System.out.println("Ââåäèòå 2 ÷èñëà. Îòâåòîì áóäåò òî ÷èñëî, êîòîðîå áëèæå ê 10");
+		System.out.println("Введите 2 числа. Ответом будет то число, которое ближе к 10");
 		a = scan.nextInt();
 		b = scan.nextInt();
 		abuf = Math.abs(a - 10);
 		bbuf = Math.abs(b - 10);
 		if (abuf > bbuf)
-			System.out.println("Îòâåò: " + b);
+			System.out.println("Ответ: " + b);
 		else
-			System.out.println("Îòâåò: " + a);
+			System.out.println("Ответ: " + a);
 		scan.close();
 		
 	}
